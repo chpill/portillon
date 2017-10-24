94eb30baeba5c98634c513081a631af532b55cfc
chpill <etienne@spillemaeker.com>
2017-10-24 22:39:08 +0200

    Fix test of exanple handler

--- src/portillon/handler/example.clj
+++ src/portillon/handler/example.clj
@@ -1,6 +1,6 @@
 (ns portillon.handler.example
   (:require [ataraxy.core :as ataraxy]
-            [ataraxy.response :as response] 
+            [ataraxy.response :as response]
             [clojure.java.io :as io]
             [integrant.core :as ig]))
 
--- test/portillon/handler/example_test.clj
+++ test/portillon/handler/example_test.clj
@@ -3,13 +3,18 @@
             [kerodon.core :refer :all]
             [kerodon.test :refer :all]
             [integrant.core :as ig]
+            [duct.core :as duct]
+            [clojure.java.io :as io]
             [portillon.handler.example :as example]))
 
-(def handler
-  (ig/init-key :portillon.handler/example {}))
+(def router (let [router-key :duct.router/ataraxy]
+              (-> (duct/read-config (io/resource "portillon/config.edn"))
+                  (duct/prep [router-key])
+                  (ig/init [router-key])
+                  router-key)))
 
 (deftest smoke-test
   (testing "example page exists"
-    (-> (session handler)
+    (-> (session router)
         (visit "/example")
         (has (status? 200) "page exists"))))
